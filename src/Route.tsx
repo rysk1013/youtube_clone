fdcfa3adbb0f89b706dc53899aa562b97f9795ba
rysk1013 <0829ryosuke@gmail.com>
2023-03-30 18:51:38 +0900

    add routing

---
+++ src/Route.tsx
@@ -0,0 +1,39 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+import { HomeLayout } from './layouts/Home';
+import { SideLessHomeLayout } from './layouts/SideLessHome';
+import { SimpleLayout } from './layouts/Simpole';
+
+export const RootRouter = () => {
+  return useRoutes([
+    {
+      // HeaderとSidebarがあるデザインのページ
+      // elementに指定したコンポーネントをページのレイアウトデザインとして使用する
+      element: <HomeLayout />,
+      // childrenでは、pathに指定したURLで、使用するコンポーネントを指定する
+      children: [{ path: '/', element: <div>Home</div>}],
+    },
+    {
+      // Headerのみのデザインのページ
+      element: <SideLessHomeLayout />,
+      children: [
+        { path: 'watch', element: <Navigate to='/' /> },
+        { path: 'watch/:videoId', element: <div>watch</div> }
+      ],
+    },
+    {
+      // HeaderもSidebarもないページのデザイン
+      element: <SimpleLayout />,
+      children: [
+        { path: 'login', element: <div>ログイン</div> },
+        { path: 'signup', element: <div>新規作成</div> },
+        { path: 'forget', element: <div>パスワードリセット</div> },
+        { path: '404', element: <div>Not Found</div> },
+
+        // pathに"*"を指定することで、「全て」のURLとして指定する
+        // Navigateを指定することで、リダイレクト処理
+        { path: '*', element: <Navigate to='/404' /> },
+      ],
+    },
+    { path: '*', element: <Navigate to='/404' /> },
+  ]);
+};
