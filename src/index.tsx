f4391987913aba1ca3d1bd1c45b17878591dc67f
rysk1013 <0829ryosuke@gmail.com>
2023-03-30 19:17:29 +0900

    add src/index

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// BrowserRouterをインポート
+import { BrowserRouter } from 'react-router-dom';
+import { RootRouter } from './Route';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <RootRouter />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
