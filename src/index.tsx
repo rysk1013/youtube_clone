d69c9cf44dfcdec185bffe5ff247266a3630f035
rysk1013 <0829ryosuke@gmail.com>
2023-03-31 18:18:58 +0900

    settings for using material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 // BrowserRouterをインポート
 import { BrowserRouter } from 'react-router-dom';
 import { RootRouter } from './Route';
+// Material-UIの設定類をインポート
+import { createTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+// Material-UIの「テーマ」を作成する
+// Material-UIのテーマ（色など）をカスタマイズする際には、createThemeの引数にカスタマイズ項目を渡す
+// 今回は何もカスタマイズしないので、何も指定していない
+const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RootRouter />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        {/*  ブラウザの違いを吸収し、どのデバイスでも同じように表示する用のCSSを使用する */}
+        <CssBaseline />
+        <RootRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
