408498adc646f3f17eb6ed1e9096aef864d46ee4
rysk1013 <0829ryosuke@gmail.com>
2023-03-31 19:33:34 +0900

    add header avatar icon

--- src/templates/DashboardHeader/index.tsx
+++ src/templates/DashboardHeader/index.tsx
@@ -1,5 +1,6 @@
-import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import { AppBar, Avatar, IconButton, Toolbar } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
 import { Logo } from '../../components/Logo';
 import { SearchBar } from './SearchBar';
 // export defaultしているので、import側でuseStylesと命名
@@ -13,17 +14,29 @@ export const DashboardHeader = () => {
     // color="inherit" : 背景を白色に
     // elevation={0} : 影(box-shadow)を無くす
     <AppBar elevation={0} color='inherit'>
-      <Toolbar>
-        {/* アイコン用のボタンを配置 */}
-        <IconButton>
-          <MenuIcon />
-        </IconButton>
-        {/* ロゴを配置 */}
-        <div className={styles.logo}>
-          <Logo />
+      <Toolbar className={styles.between}>
+        <div className={styles.flex}>
+          {/* アイコン用のボタンを配置 */}
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          {/* ロゴを配置 */}
+          <div className={styles.logo}>
+            <Logo />
+          </div>
         </div>
         {/* 検索バー */}
         <SearchBar />
+        <div className={styles.flex}>
+          {/* 新規動画作成アイコンボタン */}
+          <IconButton>
+            <VideoCallIcon />
+          </IconButton>
+          {/* プロフィールアイコン */}
+          <IconButton className={styles.profileIcon}>
+            <Avatar />
+          </IconButton>
+        </div>
       </Toolbar>
     </AppBar>
   );
