ae0c4fb404fd1871a04a281a6de596e8bda9b2e4
rysk1013 <0829ryosuke@gmail.com>
2023-04-08 17:55:50 +0900

    fix line height

--- src/components/VideoCard/SubHeaderContent/index.tsx
+++ src/components/VideoCard/SubHeaderContent/index.tsx
@@ -1,10 +1,12 @@
 import { Typography } from '@material-ui/core';
+import useStyles from './style';
 
 export const SubHeaderContent = () => {
+  const styles = useStyles();
   return (
     <>
-      <Typography variant='body2'>Figma</Typography>
-      <Typography variant='body2'>16K views 2 months ago</Typography>
+      <Typography className={styles.root} variant='body2'>Figma</Typography>
+      <Typography className={styles.root} variant='body2'>16K views 2 months ago</Typography>
     </>
   );
 };
