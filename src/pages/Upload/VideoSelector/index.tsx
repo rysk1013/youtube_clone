7392edd37399c0ffcd7daca0c55bef433c033dbe
rysk1013 <0829ryosuke@gmail.com>
2023-04-16 18:37:56 +0900

    process holding file

--- src/pages/Upload/VideoSelector/index.tsx
+++ src/pages/Upload/VideoSelector/index.tsx
@@ -1,7 +1,16 @@
-import { useRef } from 'react';
+import { useState, useRef, ChangeEvent } from 'react';
 import { Button } from '@material-ui/core';
 
 export const VideoSelect = () => {
+  const [file, setFile] = useState<File>();
+
+  // 「ファイルを選択」した後に、選択されたファイルをfileに代入する処理
+  const selectedFile = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.files?.length) {
+      setFile(event.currentTarget.files[0]);
+    }
+  };
+
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleClick = () => {
@@ -14,10 +23,10 @@ export const VideoSelect = () => {
         Buttonをクリックしたらファイルを選択する仕様にするため、<input type="file" />を指定
         hiddenを指定して非表示にしている
       */}
-      <input type="file" hidden ref={inputRef} />
-      <Button onClick={handleClick}>
-        ファイルを選択
-      </Button>
+      <input type="file" hidden ref={inputRef} onChange={selectedFile} />
+      {/* ファイルが選択されたら、名前を表示する */}
+      {file?.name}
+      <Button onClick={handleClick}>ファイルを選択</Button>
     </div>
   );
 };
